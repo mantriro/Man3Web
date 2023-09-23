f51e71b62ebe4ea1a5f0d8eaa690bc5396c0187a
mantriro <mrohit58@gmail.com>
2023-09-23 14:59:38 -0400

    Added db context constructor and ef obj implementation Added model builder, added migration update for category table Change primar key to CategoryId from Id Layout file- theme from bootstrap watch Added proper Connection string in appsettings Added EF db context code in Program.cs

---
+++ Man3Web/Controllers/CategoryController.cs
@@ -0,0 +1,28 @@
+﻿using Man3Web.Data;
+using Man3Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Man3Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+
+        }
+
+        public IActionResult Index()
+        {
+
+            List<Category> objCatList= _db.Categories.ToList();    
+            return View(objCatList);
+        }
+
+   
+
+    }
+}
---
+++ Man3Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Man3Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Man3Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { CategoryId= 1, Name = "Action", DisplayOrder= 1},
+                new Category { CategoryId = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category { CategoryId = 3, Name = "Drama", DisplayOrder = 3 }
+                );
+        }
+    }
+}
