9fa3a0e2e2dd0ba0b222c6763e011513eb4c6635
mantriro <mrohit58@gmail.com>
2023-10-29 03:56:11 -0400

    Added pending migrations

--- Man3Web/Migrations/20231029075532_355AM.Designer.cs
+++ Man3Web/Migrations/20231029075532_355AM.Designer.cs
@@ -3,6 +3,7 @@ using Man3Web.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Man3Web.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231029075532_355AM")]
+    partial class _355AM
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
